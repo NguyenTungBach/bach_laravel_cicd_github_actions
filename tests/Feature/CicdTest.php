cdd92d91964e605d61c9ae1dbb03b33293cbd61b
bach nguyen <nguyentungbachholo@gmail.com>
2024-02-26 17:37:16 +0700

    bach update test cicd actions 2

--- tests/Feature/CicdTest.php
+++ tests/Feature/CicdTest.php
@@ -15,7 +15,7 @@ class CicdTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('api/cicd');
+        $response = $this->get('api/cicd_error');
 
         $response->assertStatus(200);
     }
